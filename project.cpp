ec8d8dddd438ab315c839933315cc14e795241ee
Auyon-ECE-2410043 <auyondip@gmail.com>
2026-04-12 23:55:33 +0600

    Auyon Vai

--- project.cpp
+++ project.cpp
@@ -244,6 +244,193 @@ public:
         return false;
     }
 };
+class Hotel : public AdminBase, public Customer
+{
+private:
+    vector<Room> rooms;
+    vector<Customer> customers;
+
+public:
+    Hotel()
+    {
+    }
+
+    void addRoom()
+    {
+
+        int no = safeInt("Enter Room No: ");
+        for (size_t i = 0; i < rooms.size(); i++)
+        {
+            if (rooms[i].getRoomNo() == no)
+            {
+                cout << "Room exists!\n";
+                return;
+            }
+        }
+        string type;
+        cout << "Enter Type ( Single / Double) :";
+        cin >> type;
+        float price = safeFloat("Enter Price: ");
+        rooms.push_back(Room(no, type, price));
+        cout << "Room added successfully!\n";
+    }
+
+    void displayRooms() const
+    {
+        cout << left << setw(10) << "Room"
+             << setw(10) << "Type"
+             << setw(10) << "Price"
+             << setw(10) << "Status" << endl;
+
+        displayVector(rooms);
+    }
+
+    void bookRoom()
+    {
+        int id;
+        while (true)
+        {
+            id = safeInt("Enter Customer ID: ");
+            bool exists = false;
+            for (const auto &c : customers)
+            {
+                if (c.getCustID() == id)
+                {
+                    exists = true;
+                    break;
+                }
+            }
+
+            if (exists)
+            {
+                cout << "Customer ID already exists! Enter a different ID.\n";
+            }
+            else
+            {
+                break;
+            }
+        }
+        // int id = safeInt("Enter Customer ID: ");
+
+        string name;
+        cout << "Enter Name: ";
+        getline(cin, name);
+
+        int roomNo = safeInt("Enter Room No: ");
+        int days = safeInt("Enter Days: ");
+
+        for (size_t i = 0; i < rooms.size(); i++)
+        {
+            if (rooms[i].getRoomNo() == roomNo && !rooms[i].getStatus())
+            {
+                rooms[i].book();
+                customers.push_back(Customer(id, name, roomNo, days));
+                cout << " Room booked successfully!\n";
+                return;
+            }
+        }
+        cout << "Room not available!\n";
+    }
+
+    void checkOut()
+    {
+        int room = safeInt("Enter Room No: ");
+
+        for (size_t i = 0; i < rooms.size(); i++)
+        {
+            if (rooms[i].getRoomNo() == room && rooms[i].getStatus())
+            {
+                for (size_t j = 0; j < customers.size(); j++)
+                {
+                    if (customers[j].getRoomNo() == room)
+                    {
+                        Bill b;
+                        b.calculate(customers[j], rooms[i].getPrice(), 0);
+
+                        cout << "\n========== BILL ==========\n";
+                        cout << "Customer: " << customers[j].getName() << endl;
+                        cout << "Room Bill: " << b.roomBill << endl;
+                        cout << "Food Bill: " << b.foodBill << endl;
+                        cout << "Total: " << b.totalBill << endl;
+
+                        customers.erase(customers.begin() + j);
+                        rooms[i].free();
+                        return;
+                    }
+                }
+            }
+        }
+
+        cout << "Invalid Room or Not Booked!\n";
+    }
+    void displayCustomers() const
+    {
+        if (customers.empty())
+        {
+            cout << "No customers in the hotel at this moment.\n";
+            return;
+        }
+        displayVector(customers);
+    }
+
+    void searchRoom() const
+    {
+        int num = safeInt("Enter Room No: ");
+        for (size_t i = 0; i < rooms.size(); i++)
+        {
+            if (rooms[i].getRoomNo() == num)
+            {
+                rooms[i].displayInfo();
+                return;
+            }
+        }
+        cout << "Room not found!\n";
+    }
+
+    void run()
+    {
+        int choice;
+        while (true)
+        {
+            cout << "\n1.Add Room\n2.Book Room\n3.Check Out\n4.Display Rooms\n5.Display Customers\n6.Search Room\n7.Exit\nEnter choice: ";
+            choice = safeInt();
+            switch (choice)
+            {
+            case 1:
+                addRoom();
+                break;
+            case 2:
+                bookRoom();
+                break;
+            case 3:
+                checkOut();
+                break;
+            case 4:
+                displayRooms();
+                break;
+            case 5:
+                displayCustomers();
+                break;
+            case 6:
+                searchRoom();
+                break;
+            case 7:
+            {
+                string confirm;
+                cout << "Exit? (YES/NO): ";
+                cin >> confirm;
+                if (confirm == "yes" || confirm == "YES")
+                    return;
+                break;
+            }
+
+            default:
+                cout << "Invalid choice!\n";
+            }
+        }
+    }
+};
+
 // ---------- MAIN ----------
 int main()
 {
